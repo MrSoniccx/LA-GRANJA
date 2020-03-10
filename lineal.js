911870dd0b4d233d64cc0f98ee0071cd978b21ca
Edgar Gonzalez <edgar_gm6@hotmail.com>
2020-03-09 22:41:53 -0600

    Lineal v1

--- lineal.js
+++ lineal.js
@@ -1,7 +1,26 @@
 class Lineal {
-    constructor(x0, x, x1) {
-        this.x0 = x0
-        this.x = x
-        this.x1 = x1
-    }
+  /**
+   *
+   * @param {Number} x0
+   * @param {Number} x
+   * @param {Number} x1
+   */
+  constructor(x0, x, x1) {
+    this.x0 = x0;
+    this.x = x;
+    this.x1 = x1;
+  }
+
+  operacion() {
+    let fx0 = Math.log(this.x0);
+    let fx = Math.log(this.x);
+    let fx1 = Math.log(this.x1);
+    let formula1 = Number(((this.x - this.x1) / (this.x0 - this.x1)) * fx0);
+    let formula2 = Number(((this.x - this.x0) / (this.x1 - this.x0)) * fx1);
+    let aproximacion = formula1 + formula2;
+    return aproximacion;
+  }
 }
+
+let test = new Lineal(0.5, 1, 1.5);
+console.log(test.operacion());
--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 # Interpolacion de Lagrange
 
 <img src="resources/lagrange.jpg" width="700">
+
+## Como usar el programa
+-  ........
